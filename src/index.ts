2cfe3ca5ab6edc4a83ea8fba457833e59ecd5e1c
survivorbat <djbatcat@gmail.com>
2022-05-16 15:29:00 +0200

    Remove message listening

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,5 @@ client.on("ready", async () => {
   console.log(`Logged in as ${client.user?.tag}!`)
 })
 
-client.on("messageCreate", async (msg) => {
-  if (msg.author.id === client.user.id) {
-    return
-  }
-
-  const change = Math.ceil(Math.random() * 3);
-  console.log(`Value of '${change}' for: ${msg.content}`);
-
-  if (change <= 2) {
-    return
-  }
-
-  await msg.reply('I love you <3')
-})
-
 client.login(process.env.DISCORD_TOKEN!)
   .catch((err) => console.error(err))
